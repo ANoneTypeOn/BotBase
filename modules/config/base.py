f93e1e5ecadeeb66ca691ab962245d41b61cd408
ANoneTypeOn <128624324+ANoneTypeOn@users.noreply.github.com>
2023-03-26 18:15:01 +0100

    Added comments to basic and TOML configuration implementations. Added placeholders to basic configuration class implementation.

--- modules/config/base.py
+++ modules/config/base.py
@@ -2,18 +2,43 @@ from typing import AnyStr, Any
 
 
 class BaseConfig:
+    """Basic class to make your own compatible configuration module."""
     _config_files: list[AnyStr]
 
     _config: dict
 
     def _get_raw(self, section: str, option: str) -> Any:
+        """This method returns raw (as it is in config) representation of config value."""
         return self._config[section][option]
 
     def get_str(self, section: str, option: str) -> str:
+        """This method returns string representation of config value."""
         return str(self._get_raw(section, option))
 
     def get_int(self, section: str, option: str) -> int:
+        """This method returns int representation of config value."""
         return int(self._get_raw(section, option))
 
     def get_bool(self, section: str, option: str) -> bool:
+        """This method returns boolean representation of config value."""
         return bool(self._get_raw(section, option))
+
+    def get_config(self) -> dict:
+        """This method returns private variable that contains values from config files."""
+        return self._config
+
+    def get_configs(self) -> list[AnyStr]:
+        """This method should get all the config files from configs dir.
+        Overload it, and write it as you want."""
+
+        raise NotImplementedError("""This is method of base class. It don't have any functionality in it. 
+
+        Use this base class to create your own configuration class.""")
+
+    def update_config(self) -> dict:
+        """This method should add parsed values from config files to _config private variable.
+        Overload it, and write it as you want."""
+
+        raise NotImplementedError("""This is method of base class. It don't have any functionality in it. 
+
+        Use this base class to create your own configuration class.""")
--- modules/config/toml.py
+++ modules/config/toml.py
@@ -1,14 +1,13 @@
 import tomllib
 
 from glob import glob
-
 from typing import AnyStr
 
 from .base import BaseConfig
 
 
 class TomlConfig(BaseConfig):
-    """Configuration """
+    """Configuration module that takes .toml files from dir "configs" and drops its values to config dict."""
     def __init__(self):
         self._config = dict()
         self._config_files = list()
@@ -18,6 +17,7 @@ class TomlConfig(BaseConfig):
         self.update_config()
 
     def get_configs(self) -> list[AnyStr]:
+        """This method takes all the config files from configs dir and adds it to private config files path list."""
         configs = glob("configs/*.toml", recursive=True)
 
         if not configs:    # is list empty
@@ -26,10 +26,8 @@ class TomlConfig(BaseConfig):
             self._config_files.extend(configs)
             return configs
 
-    def get_config(self) -> dict:
-        return self._config
-
     def update_config(self) -> dict:
+        """This method adds parsed values from config files to _config private variable."""
         for config in self._config_files:
             with open(config, "rb") as config_stream:   # Any TOML file must be opened in binary mode
                 self._config.update(tomllib.load(config_stream))
